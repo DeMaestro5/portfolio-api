b86b57e9170cd5928716c59432c4928dafb7bb27
stephen <osstephen70@gmail.com>
2025-09-21 21:11:23 +0100

    first commit

---
+++ src/config.ts
@@ -0,0 +1,20 @@
+// Mapper for environment variables
+export const environment = process.env.NODE_ENV;
+export const port = process.env.PORT;
+export const timezone = process.env.TZ;
+
+export const corsUrl = process.env.CORS_URL;
+
+export const logDirectory = process.env.LOG_DIR;
+
+export const redis = {
+  host: process.env.REDIS_HOST || '',
+  port: parseInt(process.env.REDIS_PORT || '0'),
+  password: process.env.REDIS_PASSWORD || '',
+};
+
+export const caching = {
+  contentCacheDuration: parseInt(
+    process.env.CONTENT_CACHE_DURATION_MILLIS || '600000',
+  ),
+};
